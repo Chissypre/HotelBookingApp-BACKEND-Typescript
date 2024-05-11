b97981b286177f68398432906b2f3f5e31ce036b
Chissypre <127617061+Chissypre@users.noreply.github.com>
2024-05-11 20:42:56 +0100

    auth for login

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,21 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import authRoutes from "./routes/auth";
-import userRoutes from "./routes/users";
-import cookieParser from 'cookie-parser'
+import userRoutes from "./routes/register";
+import cookieParser from "cookie-parser";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin:process.env.FRONTEND_URL,
-  credentials:true,
-  }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
